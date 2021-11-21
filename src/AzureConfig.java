a63088d142a4ea2a15d46dad1397a9e0e33e154c
Rodrigo Ribeiro <jr.rodrigo.pt@gmail.com>
2021-11-21 14:47:20 +0000

    refactor app name as arg

---
+++ src/AzureConfig.java
@@ -0,0 +1,90 @@
+import com.microsoft.azure.management.resources.fluentcore.arm.Region;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public class AzureConfig {
+
+    // Auth file location
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
+    // TODO: This file should be created by running in the console:
+    // az ad sp create-for-rbac --sdk-auth > azure.auth
+    public final String AZURE_AUTH_LOCATION;
+
+    // TODO: These variable allow you to control what is being created
+    public final boolean CREATE_STORAGE;
+    public final boolean CREATE_COSMOSDB;
+    public final boolean CREATE_REDIS;
+
+    // TODO: change your suffix and other names if you want
+    public final String MY_PREFIX;
+    public final String MY_SUFFIX;
+
+    public final String AZURE_COSMOSDB_NAME;	// Cosmos DB account name
+    public final String AZURE_COSMOSDB_DATABASE;	// Cosmos DB database name
+    public final String[] BLOB_CONTAINERS;	// Containers to add to the blob storage
+
+    public final Region[] REGIONS; // Define the regions to deploy resources here
+
+    // Name of resource group for each region
+    public final String[] AZURE_RG_REGIONS;
+
+    // Name of application server to be launched in each regions -- launching the application
+    // server must be done using mvn, as you have been doing
+    // TODO: this name should be the same as defined in your app
+    public final String[] AZURE_APP_NAME;
+
+    // Name of Blob storage account
+    public final String[] AZURE_STORAGE_NAME;
+
+    // Name of Redis server to be defined
+    public final String[] AZURE_REDIS_NAME;
+
+    // Name of Azure functions to be launched in each regions
+    public final String[] AZURE_FUNCTIONS_NAME;
+
+    // Name of property file with keys and URLS to access resources
+    public final String[] AZURE_PROPS_LOCATIONS ;
+
+    // Name of shell script file with commands to set application setting for you application server
+    // and Azure functions
+    public final String[] AZURE_SETTINGS_LOCATIONS;
+
+    public AzureConfig(String suffix) {
+        AZURE_AUTH_LOCATION = "azure.auth";
+
+       CREATE_STORAGE = true;
+        CREATE_COSMOSDB = true;
+        CREATE_REDIS = true;
+
+        MY_PREFIX= "scc2122";
+        MY_SUFFIX = Optional.ofNullable(suffix).orElse("adrqrd");
+
+        AZURE_COSMOSDB_NAME = MY_PREFIX + MY_SUFFIX;
+        AZURE_COSMOSDB_DATABASE = MY_PREFIX + "db" + MY_SUFFIX;
+        BLOB_CONTAINERS = new String[]{"images"};
+
+        REGIONS = new Region[] { Region.EUROPE_WEST };
+
+        AZURE_RG_REGIONS = Arrays.stream(REGIONS)
+                .map(reg -> MY_PREFIX + "-rg-" + reg.name() + "-" + MY_SUFFIX).toArray(String[]::new);
+
+        AZURE_APP_NAME = Arrays.stream(REGIONS).map(reg -> MY_PREFIX + "-app-" + reg.name() + "-" + MY_SUFFIX)
+                .toArray(String[]::new);
+
+        AZURE_STORAGE_NAME = Arrays.stream(REGIONS).map(reg -> "sccstore" + reg.name() + MY_SUFFIX)
+                .toArray(String[]::new);
+
+        AZURE_REDIS_NAME = Arrays.stream(REGIONS).map(reg -> "redis" + reg.name() + MY_SUFFIX)
+                .toArray(String[]::new);
+
+        AZURE_FUNCTIONS_NAME = Arrays.stream(REGIONS).map(reg -> MY_PREFIX + "fun" + reg.name() + MY_SUFFIX)
+                .toArray(String[]::new);
+
+        AZURE_PROPS_LOCATIONS = Arrays.stream(REGIONS)
+                .map(reg -> "azurekeys-" + reg.name() + ".props").toArray(String[]::new);
+
+        AZURE_SETTINGS_LOCATIONS = Arrays.stream(REGIONS)
+                .map(reg -> "azureprops-" + reg.name() + ".sh").toArray(String[]::new);
+    }
+}
--- src/AzureManagement.java
+++ src/AzureManagement.java
@@ -35,58 +35,128 @@ import com.microsoft.azure.management.storage.StorageAccountSkuType;
 import com.microsoft.rest.LogLevel;
 
 public class AzureManagement {
-	// Auth file location
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
-	// TODO: This file should be created by running in the console:
-	// az ad sp create-for-rbac --sdk-auth > azure.auth
-	static final String AZURE_AUTH_LOCATION = "azure.auth";
 
-	// TODO: These variable allow you to control what is being created
-	static final boolean CREATE_STORAGE = true;
-	static final boolean CREATE_COSMOSDB = true;
-	static final boolean CREATE_REDIS = true;
+	public static void main(String[] args) {
+		try {
+			AzureConfig CONFIG = new AzureConfig(args[0]);
 
-	// TODO: change your suffix and other names if you want
-	static final String MY_PREFIX= "scc2122"; // Add your suffix here
-	static final String MY_SUFFIX = "adrqrd"; // Add your suffix here
+			final Map<String, Map<String, String>> props = new HashMap<String, Map<String, String>>();
+			Arrays.stream(CONFIG.REGIONS).forEach(reg -> props.put(reg.name(), new HashMap<String, String>()));
 
-	public static final String AZURE_COSMOSDB_NAME = MY_PREFIX + MY_SUFFIX;	// Cosmos DB account name
-	public static final String AZURE_COSMOSDB_DATABASE = MY_PREFIX + "db" + MY_SUFFIX;	// Cosmos DB database name
-	public static final String[] BLOB_CONTAINERS = { "images" };	// Containers to add to the blob storage
+			List<Thread> threads = new ArrayList<Thread>();
 
-	static final Region[] REGIONS = new Region[] { Region.EUROPE_WEST }; // Define the regions to deploy resources here
-	
-	// Name of resource group for each region
-	static final String[] AZURE_RG_REGIONS = Arrays.stream(REGIONS)
-			.map(reg -> MY_PREFIX + "-rg-" + reg.name() + "-" + MY_SUFFIX).toArray(String[]::new);
+			final Azure azure = createManagementClient(CONFIG.AZURE_AUTH_LOCATION);
+			if (args.length >= 2 && args[1].equalsIgnoreCase("-delete")) {
+				Arrays.stream(CONFIG.AZURE_RG_REGIONS).forEach(reg -> deleteResourceGroup(azure, reg));
+			} else {
+				// Init properties files
+				for (String propF : CONFIG.AZURE_PROPS_LOCATIONS) {
+					Files.deleteIfExists(Paths.get(propF));
+					Files.write(Paths.get(propF),
+							("# Date : " + new SimpleDateFormat().format(new Date()) + "\n").getBytes(),
+							StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+				}
+				// Init settings files
+				for (String propF : CONFIG.AZURE_SETTINGS_LOCATIONS) {
+					Files.deleteIfExists(Paths.get(propF));
+					Files.write(Paths.get(propF), "".getBytes(), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+				}
 
-	// Name of application server to be launched in each regions -- launching the application
-	// server must be done using mvn, as you have been doing
-	// TODO: this name should be the same as defined in your app
-	static final String[] AZURE_APP_NAME = Arrays.stream(REGIONS).map(reg -> MY_PREFIX + "-app-" + reg.name() + "-" + MY_SUFFIX)
-			.toArray(String[]::new);
+				// Create resource groups
+				for (int i = 0; i < CONFIG.REGIONS.length; i++)
+					createResourceGroup(azure, CONFIG.AZURE_RG_REGIONS[i], CONFIG.REGIONS[i]);
 
-	// Name of Blob storage account
-	static final String[] AZURE_STORAGE_NAME = Arrays.stream(REGIONS).map(reg -> "sccstore" + reg.name() + MY_SUFFIX)
-			.toArray(String[]::new);
-	
-	// Name of Redis server to be defined
-	static final String[] AZURE_REDIS_NAME = Arrays.stream(REGIONS).map(reg -> "redis" + reg.name() + MY_SUFFIX)
-			.toArray(String[]::new);
-		
-	// Name of Azure functions to be launched in each regions
-	static final String[] AZURE_FUNCTIONS_NAME = Arrays.stream(REGIONS).map(reg -> MY_PREFIX + "fun" + reg.name() + MY_SUFFIX)
-			.toArray(String[]::new);
+				if (CONFIG.CREATE_STORAGE) {
+					Thread th = new Thread(() -> {
+						try {
+							final Azure azure0 = createManagementClient(CONFIG.AZURE_AUTH_LOCATION);
+							for (int i = 0; i < CONFIG.REGIONS.length; i++) {
+								StorageAccount accountStorage = createStorageAccount(azure0, CONFIG.AZURE_RG_REGIONS[i],
+										CONFIG.AZURE_STORAGE_NAME[i], CONFIG.REGIONS[i]);
+								dumpStorageKey(props.get(CONFIG.REGIONS[i].name()), CONFIG.AZURE_PROPS_LOCATIONS[i],
+										CONFIG.AZURE_SETTINGS_LOCATIONS[i], CONFIG.AZURE_APP_NAME[i], CONFIG.AZURE_FUNCTIONS_NAME[i],
+										CONFIG.AZURE_RG_REGIONS[i], accountStorage);
+								for (String cont : CONFIG.BLOB_CONTAINERS)
+									createBlobContainer(azure0, CONFIG.AZURE_RG_REGIONS[i], CONFIG.AZURE_STORAGE_NAME[i], cont);
+							}
+							System.err.println("Azure Blobs Storage resources created with success");
 
-	// Name of property file with keys and URLS to access resources
-	static final String[] AZURE_PROPS_LOCATIONS = Arrays.stream(REGIONS)
-			.map(reg -> "azurekeys-" + reg.name() + ".props").toArray(String[]::new);
-	
-	// Name of shell script file with commands to set application setting for you application server
-	// and Azure functions
-	static final String[] AZURE_SETTINGS_LOCATIONS = Arrays.stream(REGIONS)
-			.map(reg -> "azureprops-" + reg.name() + ".sh").toArray(String[]::new);
-		
+						} catch (Exception e) {
+							System.err.println("Error while creating storage resources");
+							e.printStackTrace();
+						}
+						return;
+					});
+					th.start();
+					threads.add(th);
+				}
+
+				if (CONFIG.CREATE_COSMOSDB) {
+					Thread th = new Thread(() -> {
+						try {
+							final Azure azure0 = createManagementClient(CONFIG.AZURE_AUTH_LOCATION);
+							CosmosDBAccount accountCosmosDB = createCosmosDBAccount(azure0, CONFIG.AZURE_RG_REGIONS[0],
+									CONFIG.AZURE_COSMOSDB_NAME, CONFIG.REGIONS);
+							for (int i = 0; i < CONFIG.REGIONS.length; i++) {
+								dumpCosmosDBKey(props.get(CONFIG.REGIONS[i].name()), CONFIG.AZURE_PROPS_LOCATIONS[i],
+										CONFIG.AZURE_SETTINGS_LOCATIONS[i], CONFIG.AZURE_APP_NAME[i], CONFIG.AZURE_FUNCTIONS_NAME[i],
+										CONFIG.AZURE_RG_REGIONS[i], CONFIG.AZURE_COSMOSDB_DATABASE, accountCosmosDB);
+							}
+							CosmosClient cosmosClient = getCosmosClient(accountCosmosDB);
+							createCosmosDatabase(cosmosClient, CONFIG.AZURE_COSMOSDB_DATABASE);
+
+							//TODO: create the collections you have in your application
+							createCosmosCollection(cosmosClient, CONFIG.AZURE_COSMOSDB_DATABASE, "Users", "/idUser", new String[]{"/idUser"});
+							createCosmosCollection(cosmosClient, CONFIG.AZURE_COSMOSDB_DATABASE, "Channels", "/idChannel", new String[]{"/idChannel"});
+							createCosmosCollection(cosmosClient, CONFIG.AZURE_COSMOSDB_DATABASE, "Messages", "/idChannel", new String[]{"/idMessage"});
+
+							System.err.println("Azure Cosmos DB resources created with success");
+
+						} catch (Exception e) {
+							System.err.println("Error while creating cosmos db resources");
+							e.printStackTrace();
+						}
+					});
+					th.start();
+					threads.add(th);
+				}
+
+				if (CONFIG.CREATE_REDIS) {
+					Thread th = new Thread(() -> {
+						try {
+							final Azure azure0 = createManagementClient(CONFIG.AZURE_AUTH_LOCATION);
+							for (int i = 0; i < CONFIG.REGIONS.length; i++) {
+								RedisCache cache = createRedis(azure0, CONFIG.AZURE_RG_REGIONS[i], CONFIG.AZURE_REDIS_NAME[i],
+										CONFIG.REGIONS[i]);
+								dumpRedisCacheInfo(props.get(CONFIG.REGIONS[i].name()), CONFIG.AZURE_PROPS_LOCATIONS[i],
+										CONFIG.AZURE_SETTINGS_LOCATIONS[i], CONFIG.AZURE_APP_NAME[i], CONFIG.AZURE_FUNCTIONS_NAME[i],
+										CONFIG.AZURE_RG_REGIONS[i], cache);
+							}
+							System.err.println("Azure Redis resources created with success");
+						} catch (Exception e) {
+							System.err.println("Error while creating redis resources");
+							e.printStackTrace();
+						}
+					});
+					th.start();
+					threads.add(th);
+				}
+
+			}
+			for (Thread th : threads) {
+				th.join();
+			}
+		} catch (Exception e) {
+			System.err.println("Error while creating resources");
+			e.printStackTrace();
+		}
+		System.exit(0);
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////// Azure Group
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	public static Azure createManagementClient(String authFile) throws CloudException, IOException {
 		File credFile = new File(authFile);
 		Azure azure = Azure.configure().withLogLevel(LogLevel.BASIC).authenticate(credFile).withDefaultSubscription();
@@ -94,9 +164,8 @@ public class AzureManagement {
 		return azure;
 	}
 
-	public static ResourceGroup createResourceGroup(Azure azure, String rgName, Region region) {
-		ResourceGroup resourceGroup = azure.resourceGroups().define(rgName).withRegion(region).create();
-		return resourceGroup;
+	public static void createResourceGroup(Azure azure, String rgName, Region region) {
+		azure.resourceGroups().define(rgName).withRegion(region).create();
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -133,13 +202,11 @@ public class AzureManagement {
 		List<StorageAccountKey> storageAccountKeys = account.getKeys();
 		storageAccountKeys = account.regenerateKey(storageAccountKeys.get(0).keyName());
 
-		StringBuffer keyB = new StringBuffer();
-		keyB.append("DefaultEndpointsProtocol=https;AccountName=");
-		keyB.append(account.name());
-		keyB.append(";AccountKey=");
-		keyB.append(storageAccountKeys.get(0).value());
-		keyB.append(";EndpointSuffix=core.windows.net");
-		String key = keyB.toString();
+		String key = "DefaultEndpointsProtocol=https;AccountName=" +
+				account.name() +
+				";AccountKey=" +
+				storageAccountKeys.get(0).value() +
+				";EndpointSuffix=core.windows.net";
 
 		synchronized (props) {
 			props.put("BlobStoreConnection", key);
@@ -149,7 +216,7 @@ public class AzureManagement {
 			Files.write(Paths.get(propFilename), ("BlobStoreConnection=" + key + "\n").getBytes(),
 					StandardOpenOption.APPEND);
 		}
-		StringBuffer cmd = new StringBuffer();
+		StringBuilder cmd = new StringBuilder();
 		if (functionName != null) {
 			cmd.append("az functionapp config appsettings set --name ");
 			cmd.append(functionName);
@@ -215,7 +282,7 @@ public class AzureManagement {
 			props.put("COSMOSDB_DATABASE", databaseName);
 		}
 
-		StringBuffer cmd = new StringBuffer();
+		StringBuilder cmd = new StringBuilder();
 		if (appName != null) {
 			cmd.append("az functionapp config appsettings set --name ");
 			cmd.append(appName);
@@ -324,7 +391,7 @@ public class AzureManagement {
 			props.put("REDIS_KEY", redisAccessKey.primaryKey());
 			props.put("REDIS_URL", cache.hostName());
 		}
-		StringBuffer cmd = new StringBuffer();
+		StringBuilder cmd = new StringBuilder();
 		if (appName != null) {
 			cmd.append("az functionapp config appsettings set --name ");
 			cmd.append(appName);
@@ -370,121 +437,4 @@ public class AzureManagement {
 		azure.resourceGroups().deleteByName(rgName);
 	}
 
-	public static void main(String[] args) {
-		try {
-
-
-			final Map<String, Map<String, String>> props = new HashMap<String, Map<String, String>>();
-			Arrays.stream(REGIONS).forEach(reg -> props.put(reg.name(), new HashMap<String, String>()));
-
-			List<Thread> threads = new ArrayList<Thread>();
-
-
-			final Azure azure = createManagementClient(AZURE_AUTH_LOCATION);
-			if (args.length == 1 && args[0].equalsIgnoreCase("-delete")) {
-				Arrays.stream(AZURE_RG_REGIONS).forEach(reg -> deleteResourceGroup(azure, reg));
-			} else {
-				// Init properties files
-				for (String propF : AZURE_PROPS_LOCATIONS) {
-					Files.deleteIfExists(Paths.get(propF));
-					Files.write(Paths.get(propF),
-							("# Date : " + new SimpleDateFormat().format(new Date()) + "\n").getBytes(),
-							StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-				}
-				// Init settings files
-				for (String propF : AZURE_SETTINGS_LOCATIONS) {
-					Files.deleteIfExists(Paths.get(propF));
-					Files.write(Paths.get(propF), "".getBytes(), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-				}
-
-				// Create resource groups
-				for (int i = 0; i < REGIONS.length; i++)
-					createResourceGroup(azure, AZURE_RG_REGIONS[i], REGIONS[i]);
-
-				if (CREATE_STORAGE) {
-					Thread th = new Thread(() -> {
-						try {
-							final Azure azure0 = createManagementClient(AZURE_AUTH_LOCATION);
-							for (int i = 0; i < REGIONS.length; i++) {
-								StorageAccount accountStorage = createStorageAccount(azure0, AZURE_RG_REGIONS[i],
-										AZURE_STORAGE_NAME[i], REGIONS[i]);
-								dumpStorageKey(props.get(REGIONS[i].name()), AZURE_PROPS_LOCATIONS[i],
-										AZURE_SETTINGS_LOCATIONS[i], AZURE_APP_NAME[i], AZURE_FUNCTIONS_NAME[i],
-										AZURE_RG_REGIONS[i], accountStorage);
-								for (String cont : BLOB_CONTAINERS)
-									createBlobContainer(azure0, AZURE_RG_REGIONS[i], AZURE_STORAGE_NAME[i], cont);
-							}
-							System.err.println("Azure Blobs Storage resources created with success");
-
-						} catch (Exception e) {
-							System.err.println("Error while creating storage resources");
-							e.printStackTrace();
-						}
-						return;
-					});
-					th.start();
-					threads.add(th);
-				}
-
-				if (CREATE_COSMOSDB) {
-					Thread th = new Thread(() -> {
-						try {
-							final Azure azure0 = createManagementClient(AZURE_AUTH_LOCATION);
-							CosmosDBAccount accountCosmosDB = createCosmosDBAccount(azure0, AZURE_RG_REGIONS[0],
-									AZURE_COSMOSDB_NAME, REGIONS);
-							for (int i = 0; i < REGIONS.length; i++) {
-								dumpCosmosDBKey(props.get(REGIONS[i].name()), AZURE_PROPS_LOCATIONS[i],
-										AZURE_SETTINGS_LOCATIONS[i], AZURE_APP_NAME[i], AZURE_FUNCTIONS_NAME[i],
-										AZURE_RG_REGIONS[i], AZURE_COSMOSDB_DATABASE, accountCosmosDB);
-							}
-							CosmosClient cosmosClient = getCosmosClient(accountCosmosDB);
-							createCosmosDatabase(cosmosClient, AZURE_COSMOSDB_DATABASE);
-
-							//TODO: create the collections you have in your application
-							createCosmosCollection(cosmosClient, AZURE_COSMOSDB_DATABASE, "Users", "/idUser", new String[]{"/idUser"});
-							createCosmosCollection(cosmosClient, AZURE_COSMOSDB_DATABASE, "Channels", "/idChannel", new String[]{"/idChannel"});
-							createCosmosCollection(cosmosClient, AZURE_COSMOSDB_DATABASE, "Messages", "/idChannel", new String[]{"/idMessage"});
-
-							System.err.println("Azure Cosmos DB resources created with success");
-
-						} catch (Exception e) {
-							System.err.println("Error while creating cosmos db resources");
-							e.printStackTrace();
-						}
-					});
-					th.start();
-					threads.add(th);
-				}
-
-				if (CREATE_REDIS) {
-					Thread th = new Thread(() -> {
-						try {
-							final Azure azure0 = createManagementClient(AZURE_AUTH_LOCATION);
-							for (int i = 0; i < REGIONS.length; i++) {
-								RedisCache cache = createRedis(azure0, AZURE_RG_REGIONS[i], AZURE_REDIS_NAME[i],
-										REGIONS[i]);
-								dumpRedisCacheInfo(props.get(REGIONS[i].name()), AZURE_PROPS_LOCATIONS[i], 
-										AZURE_SETTINGS_LOCATIONS[i], AZURE_APP_NAME[i], AZURE_FUNCTIONS_NAME[i],
-										AZURE_RG_REGIONS[i], cache);
-							}
-							System.err.println("Azure Redis resources created with success");
-						} catch (Exception e) {
-							System.err.println("Error while creating redis resources");
-							e.printStackTrace();
-						}
-					});
-					th.start();
-					threads.add(th);
-				}
-
-			}
-			for (Thread th : threads) {
-				th.join();
-			}
-		} catch (Exception e) {
-			System.err.println("Error while creating resources");
-			e.printStackTrace();
-		}
-		System.exit(0);
-	}
 }
