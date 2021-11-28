e1a33b5e911f760b5017cb1fcb686a593c119f3f
Rodrigo Ribeiro <jr.rodrigo.pt@gmail.com>
2021-11-28 16:25:43 +0000

    bug fix

--- src/AzureManagement.java
+++ src/AzureManagement.java
@@ -99,9 +99,9 @@ public class AzureManagement {
 						createCosmosDatabase(cosmosClient, CONFIG.AZURE_COSMOSDB_DATABASE);
 
 						//TODO: create the collections you have in your application
-						createCosmosCollection(cosmosClient, CONFIG.AZURE_COSMOSDB_DATABASE, "Users", "/idUser", new String[]{"/idUser"});
-						createCosmosCollection(cosmosClient, CONFIG.AZURE_COSMOSDB_DATABASE, "Channels", "/idChannel", new String[]{"/idChannel"});
-						createCosmosCollection(cosmosClient, CONFIG.AZURE_COSMOSDB_DATABASE, "Messages", "/idChannel", new String[]{"/idMessage"});
+						createCosmosCollection(cosmosClient, CONFIG.AZURE_COSMOSDB_DATABASE, "Users", "/id", new String[]{"/id"});
+						createCosmosCollection(cosmosClient, CONFIG.AZURE_COSMOSDB_DATABASE, "Channels", "/id", new String[]{"/id"});
+						createCosmosCollection(cosmosClient, CONFIG.AZURE_COSMOSDB_DATABASE, "Messages", "/channel", new String[]{"/id"});
 
 						System.err.println("Azure Cosmos DB resources created with success");
 					} catch (Exception e) {
